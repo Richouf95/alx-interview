c6d72f2835d8b52a868552eff31f3fde5dbd9835
Richouf95 <wourichouf95@gmail.com>
2024-07-06 16:42:43 +0100

    Fix canUnlockAll()

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-
-from collections import deque
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """
-    Determine if all boxes can be opened.
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    :param boxes: List of lists, where each sublist
-        contains keys to other boxes
-    :return: True if all boxes can be opened, False otherwise
-    """
-    boxesLength = len(boxes)
-    opened = [False] * boxesLength
-    opened[0] = True  # Box 0 is always open
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    queue = deque([0])  # Start with the first box
-
-    while queue:
-        current_box = queue.popleft()
-
-        for key in boxes[current_box]:
-            if 0 <= key < boxesLength and not opened[key]:
-                opened[key] = True
-                queue.append(key)
-
-    return all(opened)
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
